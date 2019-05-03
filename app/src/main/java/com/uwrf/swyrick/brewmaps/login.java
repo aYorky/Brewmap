bc78ba0d2e6a845bd6b9b663833ef0d6a46c5933
aYorky <alexander.yorkovich@my.uwrf.edu>
2019-05-03 14:39:11 -0500

    net 0 changes?

--- app/src/main/java/com/uwrf/swyrick/brewmaps/login.java
+++ app/src/main/java/com/uwrf/swyrick/brewmaps/login.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public class login extends AppCompatActivity {
 
+    final private String email = "";
+    final private String pWord = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+
     }
 
 }
--- app/src/main/java/com/uwrf/swyrick/brewmaps/register.java
+++ app/src/main/java/com/uwrf/swyrick/brewmaps/register.java
@@ -6,6 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 
 public class register extends AppCompatActivity {
 
+    final private String fName = "";
+    final private String lName = "";
+    final private String email = "";
+    final private String pWord = "";
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
