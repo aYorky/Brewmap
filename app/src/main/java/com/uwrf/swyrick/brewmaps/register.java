8723d26a420f3e5206263741a5d326a6c42fa935
aYorky <alexander.yorkovich@my.uwrf.edu>
2019-05-03 10:30:46 -0500

    Added activity classes and linked to respective layouts

---
+++ app/src/main/java/com/uwrf/swyrick/brewmaps/register.java
@@ -0,0 +1,14 @@
+package com.uwrf.swyrick.brewmaps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class register extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
