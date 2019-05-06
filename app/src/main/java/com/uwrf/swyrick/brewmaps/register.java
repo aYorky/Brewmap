ea4e4c3ee6da5119ccea93e76e154d25e6208f56
aYorky <alexander.yorkovich@my.uwrf.edu>
2019-05-06 11:13:41 -0500

    Adding fields into register activity

--- app/src/main/java/com/uwrf/swyrick/brewmaps/register.java
+++ app/src/main/java/com/uwrf/swyrick/brewmaps/register.java
@@ -1,18 +1,34 @@
 package com.uwrf.swyrick.brewmaps;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 
 public class register extends AppCompatActivity {
 
-    final private String fName = "";
-    final private String lName = "";
-    final private String email = "";
-    final private String pWord = "";
+    /* activity_register fields and button variables  */
 
+    //First and Last names
+    final EditText firstNameEditText = (EditText) findViewById(R.id.FirstNameEditText);
+    final EditText lastNameEditText = (EditText) findViewById(R.id.LastNameEditText);
 
+    //Username
+    final EditText usernameEditText = (EditText) findViewById(R.id.UsernameEditText);
 
+    //Password
+    final EditText passwordEditText = (EditText) findViewById(R.id.PasswordEditText);
+
+    //Email
+    final EditText emailEditText = (EditText) findViewById(R.id.EmailEditText);
+
+    //Register button
+    final Button registerButton = findViewById(R.id.RegisterButton);
+
+    //confirmations
+    final EditText passwordVerEditText = (EditText) findViewById(R.id.PasswordVerEditText);
+    final EditText emailVerEditText = (EditText) findViewById(R.id.EmailVerEditText);
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
